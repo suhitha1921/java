342e5a86b831230445164875ce425acb4825b9de
suhitha1921 <43035895+suhitha1921@users.noreply.github.com>
2018-09-07 15:05:47 +0530

    Add files via upload

---
+++ scanner.java
@@ -0,0 +1,12 @@
+import java.util.*;
+  class Main {
+  public static void main(String[] args) { 
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter the a value");
+    int a=sc.nextInt();
+    System.out.println("enter the b value");
+    int b=sc.nextInt();      
+    int c=a/b;
+    System.out.println("div:"+c);
+  }
+}
