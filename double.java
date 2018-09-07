f68eb7ea560b4ed5e02fab7eb3f36b7399d614ff
suhitha1921 <43035895+suhitha1921@users.noreply.github.com>
2018-09-07 12:19:18 +0530

    Add files via upload

---
+++ double.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    int a=6;
+    double b=7, c;
+    c=a+b;
+    System.out.println("add two numbers:"+c);
+    c=a-b;
+    System.out.println("sub two numbers:"+c);
+    c=a*b;
+    System.out.println("mul two number:"+c);
+    c=a/b;
+    System.out.println("div two number:"+c);
+   }
+}
