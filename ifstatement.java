4db0a9d6a557c801685d2492af907b52f2895230
suhitha1921 <43035895+suhitha1921@users.noreply.github.com>
2018-09-18 14:49:42 +0530

    Add files via upload

---
+++ ifstatement.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class GUVI
+{
+	public static void main (String[] args) 
+	{
+      Scanner ka=new Scanner(System.in);
+      int a;
+      System.out.println("enter the value");
+      a=ka.nextInt();
+      if(a>70)
+      {
+      System.out.println("hit");
+      }
+      else
+      {
+      System.out.println("flop");
+      }
+    }
+}
