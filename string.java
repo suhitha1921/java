89ff19f03f52431ad5e0061349c723de5916fc45
suhitha1921 <43035895+suhitha1921@users.noreply.github.com>
2018-09-25 15:11:39 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+      string name,city;
+      byte age;
+      float height;
+      boolean married;
+      char sex;
+      double population;
+      System.out.println("Name:Sridevi"+name);
+      System.out.println("City:Chennai"+city);
+      System.out.println("Age:32"+age);
+      System.out.println("Height:5.3"+height);
+      System.out.println("Married:true"+married);
+      System.out.println("Sex:F"+sex);
+      System.out.println("Population:4.828.853"+ population);
+	}
+}
