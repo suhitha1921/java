1492c1d0d3257e046b6023f0f5856fb9d7cc82fd
suhitha1921 <43035895+suhitha1921@users.noreply.github.com>
2018-10-01 15:04:04 +0530

    Add files via upload

---
+++ variableopertion.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+    int a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s;
+    a=60;
+    b=20;
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=b/a;
+    g=b%a;
+    h=b++;
+    i=b--;
+    j=a&b;
+    k=a|b;
+    l=a^b;
+    m=~a;
+    n=a<<2;
+    o=a>>2;
+    p=a>>>2;
+    q=a&b;
+    r=a|b;
+    s=(a&b);
+      System.out.println("a+b:"+c);
+      System.out.println("a-b:"+d);
+      System.out.println("a*b:"+e);
+      System.out.println("b/a:"+f);
+      System.out.println("b%a:"+g);
+      System.out.println("b++:"+h);
+      System.out.println("b--:"+i);
+      System.out.println("a&b:"+j);
+      System.out.println("a|b:"+k);
+      System.out.println("a^b:"+l);
+      System.out.println("~a:"+m);
+      System.out.println("a<<2:"+n);
+      System.out.println("a>>2:"+o);
+      System.out.println("a>>>2:"+p);
+      System.out.println("a&b:"+q);
+      System.out.println("a|b:"+r);
+      System.out.println("a&b:"+s);
+      }
+}
