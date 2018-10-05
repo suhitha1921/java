a9d4bee114cdd49ce466a2f8c61ed56260c8b50d
suhitha1921 <43035895+suhitha1921@users.noreply.github.com>
2018-10-05 12:29:34 +0530

    Add files via upload

---
+++ n times.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Main
+{
+  Scanner Sc=new Scanner(System.in);
+	public static void main (String[] args)
+	{
+		for (int i = 0; i < 3; i++) 
+    {
+			System.out.println("Hello World");
+		}
+	}
+}
