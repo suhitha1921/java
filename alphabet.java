9f33bf5054acc8b74ace1adac0e2da5258e001b0
suhitha1921 <43035895+suhitha1921@users.noreply.github.com>
2018-10-05 11:51:57 +0530

    Add files via upload

---
+++ alphabet.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+class Main 
+{
+
+    public static void main(String[] args) 
+    {
+
+        char c = 'a';
+
+        if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+            System.out.println(c + " is an alphabet.");
+        else
+            System.out.println(c + " is not an alphabet.");
+    }
+}
